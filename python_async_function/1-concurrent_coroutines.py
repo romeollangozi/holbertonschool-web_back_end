6745833f48aae3dcffe5c64fffe4b053ae5a8ca1
romeollangozi <romeollangozi123@gmail.com>
2024-01-01 22:50:42 +0100

    s

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,12 +4,12 @@ Simple demonstration of
 asyncio capabilities
 '''
 import asyncio
-from typing import List
+from typing import List, Union, Any
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[Union(float, Any)]:
     '''
     wait_n should return the list of all the delays (float values).
     The list of the delays should be in
