3acbf09de90a9813f4c019d460313bb4e44a3fc8
romeollangozi <romeollangozi123@gmail.com>
2024-01-01 22:51:58 +0100

    s

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ from typing import List, Union, Any
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[Union(float, Any)]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''
     wait_n should return the list of all the delays (float values).
     The list of the delays should be in
