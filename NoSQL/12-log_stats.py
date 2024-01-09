f2b506f8c3554d5c4d4206f6923adc11b4aeadd3
romeollangozi <romeollangozi123@gmail.com>
2024-01-09 16:34:54 +0100

    s

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,7 +6,7 @@ most Methods
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    client = MongoClient("mongodb://127.0.0.1:27017")
+    client = MongoClient()
     logs_collection = client.logs.nginx
 
     print(f"{logs_collection.count_documents({})} logs")
