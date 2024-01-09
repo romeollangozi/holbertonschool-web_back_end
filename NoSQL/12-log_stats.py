45049fd4fae3eb6db3cd6ca255a7d9ade0db8c76
romeollangozi <romeollangozi123@gmail.com>
2024-01-09 16:37:25 +0100

    s

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,9 +7,10 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient()
-    logs_collection = client.logs.nginx
+    logs_collection = client.logs["nginx"]
 
     print(f"{logs_collection.count_documents({})} logs")
+    print("Methods:")
     print(f"\tmethod GET: {logs_collection.count_documents({'method': 'GET'})}")
     print(f"\tmethod POST: {logs_collection.count_documents({'method': 'POST'})}")
     print(f"\tmethod PUT: {logs_collection.count_documents({'method': 'PUT'})}")
