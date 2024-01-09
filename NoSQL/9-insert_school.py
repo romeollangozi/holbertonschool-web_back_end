7fc67543ca59c670ee478bc26c67be28f884db1e
romeollangozi <romeollangozi123@gmail.com>
2024-01-09 15:36:44 +0100

    s

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -9,4 +9,4 @@ def insert_school(mongo_collection, **kwargs):
     Function to inset a school document
     '''
 
-    return (mongo_collection.insertOne(kwargs)).inserted_id
+    return (mongo_collection.insert_one(kwargs)).inserted_id
