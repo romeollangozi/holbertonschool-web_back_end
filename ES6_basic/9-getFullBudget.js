3691d7ee78241dfe400690158c0ccb976dcba8a4
romeollangozi <romeollangozi123@gmail.com>
2024-01-19 11:55:11 +0100

    s

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,14 +1,14 @@
-import getBudgetObject from "./7-getBudgetObject.js";
+import getBudgetObject from "./7-getBudgetObject";
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
     getIncomeInDollars: (income) => {
-      return `$${income}`;
+      `$${income}`;
     },
     getIncomeInEuros: (income) => {
-      return `${income} euros`;
+      `${income} euros`;
     },
   };
 
