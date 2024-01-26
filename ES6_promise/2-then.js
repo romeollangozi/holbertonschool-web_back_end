f3959d99bc2d09d739ddbd9c63ddb2b4f28e04ae
romeollangozi <romeollangozi123@gmail.com>
2024-01-26 15:50:49 +0100

    s

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,13 @@
 /*eslint-disable*/
 export default function handleResponseFromAPI(promise) {
   promise
-    .then(() => {
-      return { status: 200, body: "success" };
+    .then((success) => {
+      return { status: 200, body: success };
     })
     .catch(() => {
       return new Error();
     })
-    .then(() => {
+    .finally(() => {
       console.log("Got a response from the API");
     });
 }
