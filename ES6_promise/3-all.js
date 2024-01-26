0919b5b2f6c7b2934baf2d3e3bd96d8586b97c27
romeollangozi <romeollangozi123@gmail.com>
2024-01-26 13:03:47 +0100

    s

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,13 @@
-import { uploadPhoto, createUser } from './utils';
+import { uploadPhoto, createUser } from "./utils";
 
-export default async function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((data) => {
-      console.log(data[0].body, data[1].firstName, data[1].lastName);
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((Response) => {
+      console.log(
+        `${Response[0].body} ${Response[1].firstName} ${Response[1].lastName}`
+      );
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log("Signup system offline"));
 }
+
+export default handleProfileSignup;
