6a03622d87f51834c2daccd4eeb10df9e4f651dc
romeollangozi <romeollangozi123@gmail.com>
2024-01-26 16:01:57 +0100

    s

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -7,5 +7,12 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ])
+  ]).then((values)=>{
+    const result = []
+    for (const value of values){
+      if(value.status === 'rejected') result.push({status: value.status, reason: value.reason.toString()})
+      else result.push(value)
+    }
+    return result
+  })
 }
