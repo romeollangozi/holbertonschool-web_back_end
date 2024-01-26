3ee092ac229982e127e1b637ad1c80ecd59b5528
romeollangozi <romeollangozi123@gmail.com>
2024-01-26 13:37:24 +0100

    s

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,7 +4,8 @@ import signUpUser from "./4-user-promise";
 import uploadPhoto from "./5-photo-reject";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then(console.log)
-    .catch(console.log);
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
 }
