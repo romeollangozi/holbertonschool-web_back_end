0df86d8eaf07e0fc27173b973ad94bdec52c6df8
romeollangozi <romeollangozi123@gmail.com>
2024-01-26 12:44:40 +0100

    s

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   promise
     .then(() => {
-      return { status: 200, body: success };
+      return { status: 200, body: 'success' };
     })
     .catch(() => {
       return new Error();
